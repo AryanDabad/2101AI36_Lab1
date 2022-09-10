38ab868d84d1045c49cc1ee32ac103a25435b5cf
AryanDabad <aryandabad@gmail.com>
2022-09-10 22:47:01 +0530

    3 Sorting Method

--- Sorting Methods.c
+++ Sorting Methods.c
@@ -94,7 +94,36 @@ void printArray(int arr[], int size)
         printf("%d ", arr[i]);
     printf("\n");
 }
- 
+ void insertionSort(int arr[], int n) 
+{ 
+    int i, key, j; 
+    for (i = 1; i < n; i++)
+    { 
+        key = arr[i]; 
+        j = i - 1; 
+  
+        // Move elements of arr[0..i-1],  
+        // that are greater than key, to one 
+        // position ahead of their 
+        // current position
+        while (j >= 0 && arr[j] > key)
+        { 
+            arr[j + 1] = arr[j]; 
+            j = j - 1; 
+        } 
+        arr[j + 1] = key; 
+    } 
+} 
+  
+// A utility function to print an array 
+// of size n 
+void printArray(int arr[], int n) 
+{ 
+    int i; 
+    for (i = 0; i < n; i++) 
+        cout << arr[i] << " "; 
+    cout << endl;
+} 
 // Driver program to test above functions
 int main()
 {
@@ -120,6 +149,11 @@ switch(ch)
     printf("\nMerge Sorted array is \n");
     printArray(arr, n);
 break;
+case 3:
+ insertionSort(arr, n); 
+    printArray(arr, n); 
+break;
+
 	default:
             printf("Error! choice is not correct");
             break;
