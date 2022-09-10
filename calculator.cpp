a2a35f0b3a510bf2613b8e5a50a2d290979eaa29
AryanDabad <aryandabad@gmail.com>
2022-09-10 22:15:36 +0530

    Calculator with Addition & SUbtraction

--- calculator.cpp
+++ calculator.cpp
@@ -13,10 +13,29 @@ int addTwoNumber(int A, int B)
 int main()
 {
     // Given two number
-    int A,B;
+cout<<"Enter operator";
+char op;
+cin>>op;
+cout<<"Enter Two Numbers : ";
+    float A,B;
     cin>>A>>B;
  
-    // Function call
-    cout << "sum = " << addTwoNumber(A, B);
+ switch (op) {
+          
+        // If user enter + 
+        case '+':
+            cout <<A+B;
+            break;
+         case '-':
+		cout<<A-B;
+		break;
+	
+
+        // If the operator is other than +, -, * or /, 
+        // error message will display
+        default:
+            cout << "Error! operator is not correct";
+            break;
+    } // switch statement ends
     return 0;
 }
