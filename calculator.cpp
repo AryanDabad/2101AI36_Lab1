fcf6011463a0d06efb8c1ad1000d1b3d3f00ddc9
AryanDabad <aryandabad@gmail.com>
2022-09-10 22:08:50 +0530

    Calculator with addition

---
+++ calculator.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+// Function to return sum
+// of two number
+int addTwoNumber(int A, int B)
+{
+    // Return sum of A and B
+    return A + B;
+}
+ 
+// Driver Code
+int main()
+{
+    // Given two number
+    int A,B;
+    cin>>A>>B;
+ 
+    // Function call
+    cout << "sum = " << addTwoNumber(A, B);
+    return 0;
+}
