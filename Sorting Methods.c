8aefa8eb62c46b2e190a123612f78549919fb3d2
AryanDabad <aryandabad@gmail.com>
2022-09-10 22:44:50 +0530

    2 Sorting Method

--- Sorting Methods.c
+++ Sorting Methods.c
@@ -7,7 +7,73 @@ void swap(int* xp, int* yp)
     *xp = *yp;
     *yp = temp;
 }
- 
+ // Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+  
+    /* create temp arrays */
+    int L[n1], R[n2];
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+  
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+  
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+  
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+  
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+  
+        merge(arr, l, m, r);
+    }
+}
 // A function to implement bubble sort
 void bubbleSort(int arr[], int n)
 {
@@ -38,15 +104,22 @@ scanf("%d",&n);
 cout<<"Enter Array : ";
 int arr[n];
 for(int i=0;i<n;i++) scanf("%d",&arr[i]);
-cout<<"Enter Number Of Sorting Method To Be Used : 1. Bubble Sort ";
+cout<<"Enter Number Of Sorting Method To Be Used : 1. Bubble Sort 2. Merge Sort";
 int ch; 
 scanf("%d",&ch);
 switch(ch)
 {
 	case 1:
 	bubbleSort(arr, n);
-    printf("Sorted array: \n");
-    printArray(arr, n)
+    printf("Bubble Sorted array: \n");
+    printArray(arr, n);
+	break;
+	case 2:
+		mergeSort(arr, 0, n - 1);
+  
+    printf("\nMerge Sorted array is \n");
+    printArray(arr, n);
+break;
 	default:
             printf("Error! choice is not correct");
             break;
